2b16c5abccd247b81cd71b7aa763c21e7ebe6057
Nicholas Farrel <nicholasfarrel2004@gmail.com>
2025-04-30 08:30:18 -0300

    Adding solution tamplate

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+const long long inf = (long long) 1e18 + 10;
+const int inf1 = (int) 1e9 + 10;
+#define int long long
+#define dbl long double
+#define endl '\n'
+#define sc second
+#define fr first
+#define mp make_pair
+#define pb push_back
+#define all(x) x.begin(), x.end()
+const int maxn = -1;
+ 
+void solve() {
+// Write your solution here
+}
+ 
+int32_t main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        solve();
+    }
+}
